b00e6a174372e9da1c7583b9b4c0895b505c8e14
Kiel Ed <kieledssh@gmail.com>
2023-09-15 10:28:20 +0300

    added src folder to gitignore, then you should build lib before using it

--- src/payments/p2wpkh.d.ts
+++
@@ -1,3 +0,0 @@
-import { Payment, PaymentOpts } from './index';
-export declare function p2wpkh(a: Payment, opts?: PaymentOpts): Payment;
-export declare function p2wpkh_old(a: Payment, opts?: PaymentOpts): Payment;
--- src/utils.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-export declare function fromHex(hex: string): Buffer;
-export declare function toHex(buffer: Uint8Array): string;
